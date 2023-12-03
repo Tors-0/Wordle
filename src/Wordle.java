cc7975c9cefc91f7a550c1c6dcd801b3a59e19d0
rae <raej0000@gmail.com>
2023-12-02 21:30:52 -0800

    project init

--- src/Wordle.java
+++ src/Wordle.java
@@ -1,3 +1,8 @@
+import org.w3c.dom.Text;
+
+import java.text.CharacterIterator;
+import java.text.StringCharacterIterator;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Wordle {
@@ -11,7 +16,17 @@ public class Wordle {
      */
     static final String[] WORDS = {"clown", "pizza", "eagle", "shady"};
     static final String randomWord = WORDS[(int)(Math.random()*WORDS.length)];
-
+    static final HashMap<Character,Integer> randomWordMap = new HashMap<>();
+    static {
+        for (int i = 0; i < randomWord.length(); i++) {
+            char currentChar = randomWord.charAt(i);
+            if (!randomWordMap.containsKey(currentChar)) {
+                randomWordMap.put(currentChar,1);
+            } else {
+                randomWordMap.put(currentChar,randomWordMap.get(currentChar) + 1);
+            }
+        }
+    }
     /*
     These variables are used to be able to change the color of the text
     In the official game, green means correct letter and position, yellow
@@ -27,12 +42,25 @@ public class Wordle {
     static Scanner scanny = new Scanner(System.in);
 
     public static void main(String[] args){
-        exampleOfMakingTextColored();
-    }
 
-    public static void exampleOfMakingTextColored(){
-        String text = "normal " + GREEN + "green" + RESET + " and " + YELLOW + "yellow" + RESET;
-        System.out.println(text);
     }
+    public static void checkString(String guess) {
+        HashMap<Character,Integer> guessMap = new HashMap<>();
+        for (int i = 0; i < guess.length(); i++) {
+            char currentChar = guess.charAt(i);
+            if (!guessMap.containsKey(currentChar)) {
+                guessMap.put(currentChar, 1);
+            } else {
+                guessMap.put(currentChar, guessMap.get(currentChar) + 1);
+            }
+        }
+        String formattedGuess = guess;
 
+    }
+    public static String green(String text) {
+        return GREEN + text + RESET;
+    }
+    public static String yellow(String text) {
+        return YELLOW + text + RESET;
+    }
 }
