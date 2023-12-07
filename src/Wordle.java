8a7ade9edeff09279af8d2a6681ff370cac567ef
rae <raej0000@gmail.com>
2023-12-06 20:44:22 -0800

    high score mechanism

--- src/Wordle.java
+++ src/Wordle.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -41,6 +43,7 @@ public class Wordle {
      */
     static void regenRandomWord() {
         int randomLength = (int)(Math.random() * Math.random() * 14) + 2;
+        randomWord = "";
         while (randomWord.length() != randomLength) {
             randomWord = WORDS.get((int) (Math.random() * WORDS.size()));
         }
@@ -64,6 +67,20 @@ public class Wordle {
     // The following two colors are used for high contrast mode to improve readability
     static final String BG_BLUE = "\u001B[46;30m";
     static final String BG_RED = "\u001B[41;97m";
+    // scrabble letter scores for scoring each word
+    static int[] scrabblePoints = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
+    public static int scrabblePoints(String text) {
+        int total = 0;
+        for (int i = 0; i < text.length(); i++) {
+            total += scrabblePoints[text.charAt(i) - 97];
+        }
+        return total;
+    }
+    // score-tracking
+    static int highScore = 0;
+    static String highScoreName;
+    static int currentScore = 0;
+    static String currentPlayerName;
     /*
     Scanner to be used throughout the program
      */
@@ -79,6 +96,7 @@ public class Wordle {
     public static void main(String[] args){
         System.out.printf("Welcome to %s-%s ! Type in a word. You have a few attempts to guess the correct word.%n",
                 green("Scrabble"), yellow("dle"));
+        gameStats();
         System.out.print("Would you like to enable high contrast? (y/n): ");
         String userChoiceHCMode = scanny.nextLine().toLowerCase();
         highContrast = !userChoiceHCMode.isEmpty() && userChoiceHCMode.charAt(0) == 'y';
@@ -86,18 +104,31 @@ public class Wordle {
         while (playing) { // keep playing the game until the user indicates they no longer wish to play
             gameOver = false;
             gameLoop();
-            System.out.println("Would you like to play again? (y/n): ");
+            if (gameOver) {
+                saveHighScore();
+                currentScore = 0;
+            }
+            System.out.print("Would you like to play again? (y/n): ");
             String selection = scanny.nextLine().toLowerCase();
             playing = !selection.isEmpty() && selection.charAt(0) == 'y';
+        }
+        saveHighScore();
+    }
+    private static void saveHighScore() {
+        if (currentScore > highScore) {
+            System.out.printf("%nYou have obtained a high score of %s, please enter your name: ", currentScore);
+            currentPlayerName = scanny.nextLine();
+            saveToFile(currentPlayerName + "|" + currentScore);
         }
     }
     public static void gameLoop() {
         regenRandomWord();
-        System.out.printf("%nType in a %s letter word. You get %s guesses. I will give you feedback on each guess...%n", randomWord.length(), randomWord.length()+1);
+        int guesses = Math.max(11 - randomWord.length(), 5);
+        System.out.printf("%nType in a %s letter word. You get %s guesses. I will give you feedback on each guess...%n", randomWord.length(), guesses);
         System.out.printf("Letters highlighted in %s are in the correct position, %n" +
                         "and letters highlighted in %s are in the word, but in the wrong position.%n",
                 green(highContrast ? "red" : "green"), yellow(highContrast ? "blue" : "yellow"));
-        for (int g = 1; g <= randomWord.length()+1 && !gameOver; g++) { // repeats 6 times for each of the users 6 guesses
+        for (int g = 1; !gameOver && g <= guesses; g++) { // repeats g times for each of the user's guesses
             System.out.printf("Turn %s: ", g);
             currentGuess = scanny.nextLine();
             currentGuess = cheatCodes();
@@ -114,7 +145,8 @@ public class Wordle {
             }
         }
         if (currentGuess.equals(randomWord) && !gameOver) {
-            System.out.println("You WIN!");
+            currentScore += scrabblePoints(randomWord);
+            System.out.printf("You WIN!\tYou gain %s points, for a total of %s points.%n", scrabblePoints(randomWord), currentScore);
         } else {
             System.out.printf("You lost =( The word was %s%n", randomWord);
         }
@@ -191,5 +223,48 @@ public class Wordle {
     public static String yellow(String text) {
         text = " " + text + " ";
         return highContrast? BG_BLUE + text + RESET : YELLOW + text + RESET;
+    }
+    // methods for high-score saving and managing
+    static File highScoreFile = new File("highscore.txt");
+    private static void saveToFile(String data) {
+        try {
+            FileWriter myWriter = new FileWriter(highScoreFile,true);
+            String text = "\n";
+            if (highScoreFile.createNewFile()) {
+                System.out.println("File created: " + highScoreFile.getName());
+                myWriter.append("// This file is for Statistical tracking and debugging purposes, please do not " +
+                        "edit, delete, or move this file //");
+            } else {
+                System.out.println("Save file already exists.");
+            }
+            myWriter.append(text);
+            myWriter.append(data);
+            System.out.println("Successfully saved game data.\n");
+            myWriter.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred: " + e);
+        }
+    }
+    public static void gameStats() {
+        String temp;
+        String name = "";
+        int score = 0;
+        try {
+            Scanner files = new Scanner(highScoreFile);
+            while (files.hasNextLine()) {
+                temp = files.nextLine();
+                if (temp.contains("|")) {
+                    name = temp.substring(0, temp.indexOf('|'));
+                    score = Integer.parseInt(temp.substring(temp.indexOf('|') + 1));
+                }
+                if (score > highScore) {
+                    highScore = score;
+                    highScoreName = name;
+                }
+            }
+            System.out.printf("The high score is currently %s, and is held by %s.%n%n", highScore, highScoreName);
+        } catch (FileNotFoundException ignored) {
+            System.out.printf("There is no current high score holder! You could be the first!%n%n");
+        }
     }
 }
